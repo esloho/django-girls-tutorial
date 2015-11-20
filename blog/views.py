08781a29788465280c1d80c75fa3ef21232ae4ee
Esther Lozano <elozano@autentia.com>
2015-11-20 19:40:02 +0100

    Added publish and remove buttons

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
     
-    if form.is_valid():
+        if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
@@ -46,3 +46,19 @@ def post_edit(request, pk):
 
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
